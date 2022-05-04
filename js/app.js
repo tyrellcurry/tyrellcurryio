f9f9016275b35cfd30287afdc761c54ec77ce7d2
Tyrell C <tyrellrcurry@gmail.com>
2022-05-03 17:41:40 -0700

    Added nav menu effects on click for desktop and mobile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
 const menuBtn = document.getElementById('menu-btn');
 const menuText = document.getElementById('menu-text');
+const nav = document.getElementById('nav');
+const hamburgerOne = document.querySelector('.hamburger-bottom');
+const hamburgerTwo = document.querySelector('.hamburger-middle');
 let menuOpen = false;
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
 menuBtn.addEventListener('click', () => {
   !menuOpen
-    ? (menuText.classList.add('menu-open'), (menuOpen = true))
-    : (menuText.classList.remove('menu-open'), (menuOpen = false));
+    ? ((menuText.style.display = 'block'),
+      hamburgerOne.classList.add('burger-open'),
+      hamburgerTwo.classList.add('burger-open2'),
+      nav.classList.add('nav-open'),
+      setTimeout(function () {
+        menuText.classList.add('menu-open');
+      }, 250),
+      (menuOpen = true))
+    : (menuText.classList.remove('menu-open'),
+      hamburgerOne.classList.remove('burger-open'),
+      hamburgerTwo.classList.remove('burger-open2'),
+      setTimeout(function () {
+        menuText.style.display = 'none';
+        nav.classList.remove('nav-open');
+      }, 250),
+      (menuOpen = false));
 });
 
 menuText.addEventListener('click', () => {
   !menuOpen
     ? (menuText.classList.add('menu-open'), (menuOpen = true))
-    : (menuText.classList.remove('menu-open'), (menuOpen = false));
+    : (menuText.classList.remove('menu-open'),
+      nav.classList.remove('nav-open'),
+      hamburgerOne.classList.remove('burger-open'),
+      hamburgerTwo.classList.remove('burger-open2'),
+      (menuOpen = false));
 });
