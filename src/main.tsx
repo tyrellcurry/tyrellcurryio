265085438bffd38e898bb8bd52b23a569ceabf04
tyrellcurry <tyrellrcurry@gmail.com>
2025-01-16 12:57:13 -0800

    Mantine UI added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
