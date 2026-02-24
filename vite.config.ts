524534a448bf0034e7abc42322b1663c27ee8ee3
tyrellcurry <tyrellcurryio@gmail.com>
2026-02-23 19:22:43 -0800

    Update config to include routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve, basename } from "path";
+import { globSync } from "fs";
+
+const htmlFiles = Object.fromEntries(
+  globSync("*.html").map((file) => [basename(file, ".html"), resolve(file)])
+);
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: htmlFiles,
+    },
+  },
 });
